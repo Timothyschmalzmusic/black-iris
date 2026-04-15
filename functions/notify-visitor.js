a41323a1afa83f0abeb5637b661d54b00f525884
Timothy Schmalz <timothy@black-iris.com>
2026-04-14 20:36:31 -0400

    Rebrand to Solstice: Jost font system, new tracks (Predator, The Faithful, Kraven, Sherwood, He Wants Justice), updated bios and gallery

--- functions/notify-visitor.js
+++ functions/notify-visitor.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
           type: 'header',
           text: {
             type: 'plain_text',
-            text: '🌐 Black Iris Website Visit'
+            text: '🌐 Solstice Website Visit'
           }
         },
         {
